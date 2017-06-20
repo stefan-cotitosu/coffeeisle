829816f731fc90394b71236b1de5e43c5f52d445
stefanc <stefan@themeisle.com>
2017-06-20 18:31:50 +0300

    Special Offer
    Link to single product added on thumbnail, product title, special offer title, product price

--- template-parts/content-offer-product.php
+++ template-parts/content-offer-product.php
@@ -6,20 +6,29 @@
  * @since 1.0.0
  */
 ?>
+
+<?php
+    global $product;
+    $current_product_url = get_permalink( $product->id );
+?>
+
 <div class="special-offer-wrapper">
 <div class="svg-container svg-block alt-shop-special-offer-top-svg">
 	<?php oblique_svg_3(); ?>
 </div>
 <div class="offer-product-wrapper">
+    <a href="<?php echo esc_attr( $current_product_url ); ?>">
 	<?php the_post_thumbnail( 'special_offer_thumbnail_size' ); ?>
+    </a>
 	<div class="offer-product-inner">
+        <a href="<?php echo esc_attr( $current_product_url ); ?>">
 		<h2 class="offer-product-special-offer"><?php echo esc_html__( 'Special Offer' ); ?></h2>
 		<h3 class="offer-product-title"><?php the_title(); ?></h3>
 		<?php
-			global $product;
 			$coffeeisle_offer_product_price = $product->get_price_html();
 		?>
 		<div class="offer-product-price"><?php if ( ! empty( $coffeeisle_offer_product_price ) ) { echo wp_kses_post( $coffeeisle_offer_product_price ); } ?></div>
+        </a>
 		<?php woocommerce_template_loop_add_to_cart(); ?>
 	</div>
 </div>
