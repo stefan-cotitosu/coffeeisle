2b13758aaa705d279de8e886c9bd4003ea9e66af
stefanc <stefan@themeisle.com>
2017-06-20 17:00:11 +0300

    Content Offer Problems
    Special Offer escaped
    the_title() instead of echo get_the_title()
    $coffeeisle_offer_product checked not to be empty and escaped with wp_kses_post

--- template-parts/content-offer-product.php
+++ template-parts/content-offer-product.php
@@ -13,13 +13,13 @@
 <div class="offer-product-wrapper">
 	<?php the_post_thumbnail( 'special_offer_thumbnail_size' ); ?>
 	<div class="offer-product-inner">
-		<h2 class="offer-product-special-offer">Special Offer</h2>
-		<h3 class="offer-product-title"><?php echo get_the_title(); ?></h3>
+		<h2 class="offer-product-special-offer"><?php echo esc_html__( 'Special Offer' ); ?></h2>
+		<h3 class="offer-product-title"><?php the_title(); ?></h3>
 		<?php
 			global $product;
 			$coffeeisle_offer_product_price = $product->get_price_html();
 		?>
-		<div class="offer-product-price"><?php echo $coffeeisle_offer_product_price ?></div>
+		<div class="offer-product-price"><?php if ( ! empty( $coffeeisle_offer_product_price ) ) { echo wp_kses_post( $coffeeisle_offer_product_price ); } ?></div>
 		<?php woocommerce_template_loop_add_to_cart(); ?>
 	</div>
 </div>
